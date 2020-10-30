2ad4217128da62c4346b112bcd46172dc728b2a1
Malavika S Menon <malavikamenon2000@gmail.com>
2020-10-31 00:27:38 +0530

    Updated HTML and JS files.

---
+++ app.js
@@ -0,0 +1,70 @@
+//Using query selector method
+//Syntax -> const element = document.querySelector(".className");
+
+const notificationElement = document.querySelector(".notification");
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+
+const key = "fd4023de2b6b6173c5583ebb2df2e2d8";
+const Kelvin = 273;
+
+const weather = {
+    temperature: {
+        unit: "celsius"
+    },
+};
+
+//weather.temperature.value =
+if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+
+} else {
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p> Browser doesn't support geolocation. </p>";
+}
+
+function setPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    getWeather(latitude, longitude);
+}
+
+function showError(error) {
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
+
+function getWeather(latitude, longitude) {
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    fetch(api, {
+            mode: 'cors'
+        })
+        .then(function(response) {
+            let data = response.json();
+            return data;
+        })
+        .then(function(data) {
+            weather.temperature.value = Math.floor(data.main.temp - Kelvin);
+            console.log("Temp", weather.temperature.value);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function() {
+            displayWeather();
+        })
+}
+
+function displayWeather() {
+    //iconElement.innerHTML = 
+
+    tempElement.innerHTML = `${weather.temperature.value}  <span> C </span>`;
+    console.log("Temp", weather.temperature.value);
+    descElement.innerHTML = weather.description;
+
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+
+}
